8872dc19bff523f33c0cbcb529e7f463c53b7e89
Java <kaabadday@gmail.com>
2020-02-28 20:50:40 +0300

    A-one

--- FamilySharedActivities/app/src/main/java/com/example/familysharedactivities/MainActivity.java
+++ FamilySharedActivities/app/src/main/java/com/example/familysharedactivities/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,16 +17,22 @@ public class MainActivity extends AppCompatActivity {
         final EditText login = findViewById(R.id.login);
         final EditText password = findViewById(R.id.password);
         Button loginBtn = findViewById(R.id.LoginBtn);
+        final TextView errorMsg = findViewById(R.id.errorMessage);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String error = "";
-                if (login.getText().toString() == ""){
+                if (login.getText().toString().equals("")){
                     error = "Заполните Логин";
                 }
-                if (password.getText().toString() == ""){
+                if (password.getText().toString().equals("")){
                     error.concat("Заполните Пароль");
 
+                }
+                if (error.equals("")){
+                    errorMsg.setText(error);
+                    errorMsg.setVisibility(View.VISIBLE);
+                    return;
                 }
             }
         });
