2e787a1864a3a89aa9165c33113898deb455c278
Java <kaabadday@gmail.com>
2020-02-28 20:40:44 +0300

    fjdhsf

--- FamilySharedActivities/app/src/main/java/com/example/familysharedactivities/MainActivity.java
+++ FamilySharedActivities/app/src/main/java/com/example/familysharedactivities/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.familysharedactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText login = findViewById(R.id.login);
+        final EditText password = findViewById(R.id.password);
+        Button loginBtn = findViewById(R.id.LoginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String error = "";
+                if (login.getText().toString() == ""){
+                    error = "Заполните Логин";
+                }
+                if (password.getText().toString() == ""){
+                    error.concat("Заполните Пароль");
 
+                }
+            }
+        });
     }
 }
